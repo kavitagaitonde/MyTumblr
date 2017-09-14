5ce1ae1bdc8dfbb593277fe3a1d2bfe522d03363
Kavita Gaitonde <kavita.gaitonde@247-inc.com>
2017-09-14 16:16:37 -0700

    Added optional requirements #1 and #2

--- MyTumblr/PostsViewController.swift
+++ MyTumblr/PostsViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import AFNetworking
 
-class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
 
     var posts: [NSDictionary] = []
     var refreshControl : UIRefreshControl?
+    var infiniteScrollActivityView:InfiniteScrollActivityView?
+    var isMoreDataLoading = false
+    var postOffset = 0
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -27,8 +30,18 @@ class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         self.refreshControl!.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         self.tableView.insertSubview(self.refreshControl!, at: 0)
         
+        // Set up Infinite Scroll loading indicator
+        let frame = CGRect(x: 0, y: tableView.contentSize.height, width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight)
+        infiniteScrollActivityView = InfiniteScrollActivityView(frame: frame)
+        infiniteScrollActivityView!.isHidden = true
+        self.tableView.addSubview(infiniteScrollActivityView!)
+        
+        var insets = self.tableView.contentInset
+        insets.bottom += InfiniteScrollActivityView.defaultHeight
+        self.tableView.contentInset = insets
+        
         // Do any additional setup after loading the view.
-        loadData()
+        loadData(0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,14 +49,16 @@ class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Dispose of any resources that can be recreated.
     }
     
-    func loadData () {
-        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+    func loadData (_ offset : Int) {
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV&offset=\(offset)")
         let request = URLRequest(url: url!)
         let session = URLSession(
             configuration: URLSessionConfiguration.default,
             delegate:nil,
             delegateQueue:OperationQueue.main
         )
+        
+        print ("Loading data with offset \(offset)........")
 
         let task : URLSessionDataTask = session.dataTask(
             with: request as URLRequest,
@@ -56,10 +71,32 @@ class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDat
                         // Recall there are two fields in the response dictionary, 'meta' and 'response'.
                         // This is how we get the 'response' field
                         let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        let postsArray : [NSDictionary] = responseFieldDictionary["posts"] as! [NSDictionary]
+                        let currCount = postsArray.count
                         
-                        // This is where you will store the returned array of posts in your posts property
-                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        if(offset == 0) {
+                            if (self.posts.count > 0) {
+                                self.posts.replaceSubrange(0...currCount-1, with:postsArray)
+                            } else {
+                                self.posts = postsArray
+                                self.postOffset=currCount
+                            }
+                        } else {
+                            if(self.posts.count <= offset) { //load more data
+                                self.posts+=postsArray
+                                self.postOffset+=currCount
+                            } else {
+                                let start = offset
+                                let end = start+currCount
+                                self.posts.replaceSubrange(start...end, with: responseFieldDictionary["posts"] as! [NSDictionary])
+                            }
+                        }
+                        self.isMoreDataLoading = false
+                        print ("Updated offset \(self.postOffset)........")
                         print(self.posts)
+                        
+                        // Stop the loading indicator
+                        self.infiniteScrollActivityView!.stopAnimating()
                         
                         self.tableView.reloadData()
                         
@@ -83,22 +120,28 @@ class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let post = self.posts[section]
+        
         let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
         headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
         
         let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
         profileView.clipsToBounds = true
         profileView.layer.cornerRadius = 15;
-        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).cgColor
+        profileView.layer.borderColor = UIColor(red:0.00, green:0.48, blue:1.00, alpha:1.0).cgColor //UIColor(white: 0.7, alpha: 0.8).cgColor
         profileView.layer.borderWidth = 1;
         
         // set the avatar
         profileView.setImageWith(NSURL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/avatar")! as URL)
         headerView.addSubview(profileView)
         
+        // Add a UILabel for the name here
+        let label = UILabel(frame: CGRect(x: 50, y: 5, width: 200, height: 25))
+        label.text = post["blog_name"] as? String
+        label.font = label.font.withSize(15)
+        headerView.addSubview(label)
+        
         // Add a UILabel for the date here
-        let label = UILabel(frame: CGRect(x: 50, y: 15, width: 200, height: 20))
-        let post = self.posts[section]
         let timestamp = post["timestamp"] as! Double
         let date = NSDate(timeIntervalSince1970: timestamp)
         let dateFormatter = DateFormatter()
@@ -106,8 +149,10 @@ class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         dateFormatter.dateStyle = DateFormatter.Style.medium //Set date style
         let timeZone = TimeZone.autoupdatingCurrent.identifier as String
         dateFormatter.timeZone = TimeZone(identifier: timeZone)
-        label.text = dateFormatter.string(from: date as Date)
-        headerView.addSubview(label)
+        let label1 = UILabel(frame: CGRect(x: 50, y: 25, width: 200, height: 20))
+        label1.text = dateFormatter.string(from: date as Date)
+        label1.font = label1.font.withSize(10)
+        headerView.addSubview(label1)
         return headerView
     }
     
@@ -134,8 +179,33 @@ class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func refreshControlAction(_ refreshControl: UIRefreshControl) {
-        loadData()
+        loadData(0)
     }
+    
+    //Scrollview Delegate methods
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            // Calculate the position of one screen length before the bottom of the results
+            let scrollViewContentHeight = tableView.contentSize.height
+            //actual hieght of the table filled in with content - height of 1 page of content
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            // When the user has scrolled past the threshold, start requesting
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+                isMoreDataLoading = true
+                print("Loading more data.......offset = \(self.postOffset)")
+                
+                // Update position of loadingMoreView, and start loading indicator
+                let frame = CGRect(x: 0, y: tableView.contentSize.height, width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight)
+                infiniteScrollActivityView?.frame = frame
+                infiniteScrollActivityView!.startAnimating()
+                
+                loadData(self.postOffset)
+            }
+            
+        }
+    }
+    
     
     // MARK: - Navigation
 
